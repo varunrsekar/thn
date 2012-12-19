0da18a86d4981c83f2445bc624062b5d698e1aba
Manikandan Saravanan <maniganz@gmail.com>
2012-12-19 15:07:19 +0530

    Created a basic signup page

--- thn/Controllers/AccountController.cs
+++ thn/Controllers/AccountController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,12 +11,51 @@ namespace thn.Controllers
 {
     public class AccountController : Controller
     {
-        static List<user> mm = new List<user>();
+        private userDBContext db = new userDBContext();
 
-        public ActionResult Register()
+        public ActionResult Index()
+        {
+            return View(db.users.ToList());
+        }
+
+        public static string encryptPassword(string password)
+        {
+            try
+            {
+                byte[] encData_byte = new byte[password.Length];
+                encData_byte = System.Text.Encoding.UTF8.GetBytes(password);
+                string encodedData = Convert.ToBase64String(encData_byte);
+                return encodedData;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error in base64Encode" + ex.Message);
+            }
+        }
+
+        public ActionResult Signup()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Signup(user user)
+        {
+            if (ModelState.IsValid)
+            {
+                user.password = encryptPassword(user.password);
+                db.users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(user);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
     }
 }
--- thn/Models/user.cs
+++ thn/Models/user.cs
@@ -2,16 +2,65 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace thn.Models
 {
     public class user
     {
+
+        [Required(ErrorMessage = "First name is required.")]
+        [Display(Name="First Name:")]
         public string firstName { get; set; }
+        [Required(ErrorMessage = "Last name is required.")]
+        [Display(Name = "Last Name:")]
         public string lastName { get; set; }
+        [Key]
+        [Required(ErrorMessage = "Email is required.")]
+        [Display(Name = "Email:")]
         public string email { get; set; }
-        public string zipcode { get; set; }
+        [Required(ErrorMessage = "Zip code is required.")]
+        [Display(Name = "Zip code:")]
+        public long zipcode { get; set; }
+        [Required(ErrorMessage = "Password is required.")]
+        [Display(Name = "Password:")]
+        [DataType(DataType.Password)]
         public string password { get; set; }
 
+    }
+
+    
+    public class viewuser
+    {
+
+        [Required(ErrorMessage = "First name is required.")]
+        [Display(Name = "First Name:")]
+        public string firstName { get; set; }
+        [Required(ErrorMessage = "Last name is required.")]
+        [Display(Name = "Last Name:")]
+        public string lastName { get; set; }
+        [Key]
+        [Required(ErrorMessage = "Email is required.")]
+        [Display(Name = "Email:")]
+        public string email { get; set; }
+        [Required(ErrorMessage = "Zip code is required.")]
+        [Display(Name = "Zip code:")]
+        public long zipcode { get; set; }
+        [Required(ErrorMessage = "Password is required.")]
+        [Display(Name = "Password:")]
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+
+        [Required(ErrorMessage = "Re enter your password")]
+        [DataType(DataType.Password)]
+        [Compare("password")]
+        [Display(Name = "Confirm password:")]
+        public string comparePassword { get; set; }
+
+    }
+    public class userDBContext : DbContext
+    {
+        public DbSet<user> users { get; set; }
     }
 }
