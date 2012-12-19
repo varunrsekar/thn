bb6562f9bb20e077b81e8fdc909d251b1ae2b8df
Rajeshwer Sekaran <rajeshwers93@gmail.com>
2012-12-19 11:45:51 +0530

    Added the user class

---
+++ thn/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using thn.Models;
+
+namespace thn.Controllers
+{
+    public class AccountController : Controller
+    {
+        static List<user> mm = new List<user>();
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ thn/Models/user.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace thn.Models
+{
+    public class user
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string zipcode { get; set; }
+        public string password { get; set; }
+
+    }
+}
