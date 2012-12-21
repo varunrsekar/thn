dbb853dd3b5a63599de42ee65b45fd2e8eb0130f
Varuna Venkatesh <varuna1510@gmail.com>
2012-12-21 11:19:51 +0530

    Model & Controller for Profile

---
+++ thn/Controllers/editProfileController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using thn.Models;
+
+namespace thn.Controllers
+{
+    public class editProfileController : Controller
+    {
+        private profileDBContext dbprof = new profileDBContext();
+
+        //
+        // GET: /EditProfile/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(profile prof)
+        {
+            profile profileExists = dbprof.profiles.Where(b => b.email == prof.email).FirstOrDefault();
+
+            if (profileExists != null)
+            {
+                dbprof.profiles.Add(prof);
+                dbprof.SaveChanges();
+                return RedirectToAction("Index", "dashboard");
+            }
+            else
+                ModelState.AddModelError("", "There is no user with this email address.");
+            return View();
+        }
+
+    }
+}
